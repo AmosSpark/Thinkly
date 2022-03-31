9ff918442681bd102106fe216acbf110e8351b2e
AmosSpark <ayobamiolugboyega@gmail.com>
2022-03-31 02:39:54 +0100

    🐛 (fix) : GET A USER
    - Allow only admins to get a user
    + Allow all looged users to get a user

--- src/resources/routes/user.route.ts
+++ src/resources/routes/user.route.ts
@@ -28,13 +28,18 @@ usersRouter.route("/auth/login").post(logUserIn);
 
 usersRouter.route("/auth/logout").get(protectRoute, logUserOut);
 usersRouter.route("/users/me").get(protectRoute, getUserProfile, getAuser);
+
 usersRouter
   .route("/users/me/change-password")
   .patch(protectRoute, changePassword);
+
 usersRouter.route("/users/me/update-me").patch(protectRoute, updateUserProfile);
+
 usersRouter
   .route("/users/me/deactivate-account")
   .delete(protectRoute, deactivateMyAccount);
+
+usersRouter.route("/users/:id").get(protectRoute, getAuser);
 
 // restrict below routes to admin only
 
@@ -42,7 +47,6 @@ usersRouter.route("/users").get(protectRoute, restrictTo("admin"), getAllUser);
 
 usersRouter
   .route("/users/:id")
-  .get(protectRoute, restrictTo("admin"), getAuser)
   .patch(protectRoute, restrictTo("admin"), updateAuser)
   .delete(protectRoute, restrictTo("admin"), deleteAuser);
 
